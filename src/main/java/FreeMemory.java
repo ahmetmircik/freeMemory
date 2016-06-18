4c4ba7e1457983ef760c6021bdaf3fa7aa3449de
Ahmet MIRCIK <ahmetmircik@gmail.com>
2016-06-18 13:35:35 +0300

    wip

--- src/main/java/FreeMemory.java
+++ src/main/java/FreeMemory.java
@@ -1,65 +1,87 @@
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.String.format;
 import static java.lang.System.out;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.locks.LockSupport.parkNanos;
 
 /**
  * Example run:
  * <p>
- * G1:
- * java -verbose:gc  -Xms2G -Xmx2G --XX:+UseG1GC -cp hazelcast/target/hazelcast-3.5.5.3.jar com.hazelcast.map.impl.eviction.FreeMemory > log_G1.txt
+ * CMS: (FAIL)
+ * java -verbose:gc  -Xms32G -Xmx32G -XX:CMSInitiatingOccupancyFraction=1 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -cp target/free-memory-issue-1.0-SNAPSHOT.jar FreeMemory
  * <p>
- * CMS:
- * java -verbose:gc  -Xms2G -Xmx2G -XX:CMSInitiatingOccupancyFraction=10 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -cp hazelcast/target/hazelcast-3.5.5.3.jar com.hazelcast.map.impl.eviction.FreeMemory > log_CMS.txt
+ * G1: (PASS)
+ * java -verbose:gc  -Xms32G -Xmx32G -XX:+UseG1GC -cp target/free-memory-issue-1.0-SNAPSHOT.jar FreeMemory
  */
 public class FreeMemory {
 
     static final int MB = 1024 * 1024;
     static final String MESSAGE = "runtime.maxMemory=%d%s, runtime.totalMemory=%d%s, runtime.freeMemory=%d%s," +
-            " runtime.usedMemory=%d%s, runTime.availableMemory=%d%s, memoryMXBean.used=%d%s, freeHeapPercentage=%.2f";
+            " runtime.usedMemory=%d%s, runTime.availableMemory=%d%s, freeHeapPercentage=%.2f";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        final ConcurrentHashMap map = new ConcurrentHashMap();
+
         Runtime runtime = Runtime.getRuntime();
         int availableProcessors = runtime.availableProcessors();
+        int threadCount = availableProcessors * 4;
 
-        for (int i = 0; i < availableProcessors * 4; i++) {
-            new Thread(new Runnable() {
+        ArrayList<Thread> threads = new ArrayList<Thread>();
 
-                ConcurrentHashMap map = new ConcurrentHashMap();
+        for (int i = 0; i < threadCount; i++) {
+            threads.add(new Thread(new Runnable() {
 
                 @Override
                 public void run() {
-                    MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-                    Runtime runtime = Runtime.getRuntime();
                     Random random = new Random();
                     while (true) {
                         final byte[] value = new byte[1024];
                         random.nextBytes(value);
                         map.put(random.nextInt(), value);
-                        printMemory(runtime, memoryMXBean);
+
+                        if (printMemoryAndBreakLoop(false)) {
+                            break;
+                        }
                     }
                 }
-            }).start();
+            }));
+        }
 
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        out.print("\n\n\nEnd of run...now we expect to see actual used-memory value\n");
+        while (true) {
+            printMemoryAndBreakLoop(true);
+            parkNanos(SECONDS.toNanos(5));
         }
     }
 
-    protected static void printMemory(Runtime runtime, MemoryMXBean memoryMXBean) {
+    protected static boolean printMemoryAndBreakLoop(boolean printAlways) {
+        Runtime runtime = Runtime.getRuntime();
+
         long maxMemory = runtime.maxMemory();
         long totalMemory = runtime.totalMemory();
         long freeMemory = runtime.freeMemory();
         long availableMemory = freeMemory + (maxMemory - totalMemory);
         double freeHeapPercentage = 100D * availableMemory / maxMemory;
-        long used = memoryMXBean.getHeapMemoryUsage().getUsed();
 
-        if (freeHeapPercentage < 12) {
+        if (printAlways || freeHeapPercentage < 12) {
             String unit = "M";
             out.println(format("[done]" + MESSAGE, toMB(maxMemory), unit, toMB(totalMemory), unit, toMB(freeMemory), unit,
-                    toMB(totalMemory - freeMemory), unit, toMB(availableMemory), unit, toMB(used), unit, freeHeapPercentage));
+                    toMB(totalMemory - freeMemory), unit, toMB(availableMemory), unit, freeHeapPercentage));
+            return true;
         }
+
+        return false;
     }
 
     private static int toMB(long bytes) {
